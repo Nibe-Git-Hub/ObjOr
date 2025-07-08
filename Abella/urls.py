e39261198dc2fc145b3a06067f2a87261a9a8e4b
yuiishiix <lzcunanan1@student.hau.edu.ph>
2025-07-08 18:42:13 +0800

    Push 1.0

--- Abella/urls.py
+++ Abella/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import hello_world, main
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', hello_world, name='hello_world'),
+    path('', main, name='main'),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
